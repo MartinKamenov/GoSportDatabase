7b0e1a9f92a460de6bf8dc49d1e2bc73ab0e2f9b
Martin Kamenov <martinkamenov10@gmail.com>
2018-05-18 12:52:09 +0300

    Fixed idGenerator.

--- GoSportDB/models/IdGenerator.js
+++ GoSportDB/models/IdGenerator.js
@@ -5,16 +5,16 @@ class IdGenerator {
                 if (users.length == 0) {
                     this.userId = 0;
                 } else {
-                    const sortedUsers = users.sort(a, b => b.id - a.id);
-                    this.userId = +sortedUsers[sortedUsers.length - 1].id + 1;
+                    const sortedUsers = users.sort((a, b) => b.id - a.id);
+                    this.userId = +sortedUsers[0].id;
                 }
                 eventRepository.getAllEvents()
                     .then((events) => {
                         if (events.length == 0) {
                             this.eventId = 0;
                         } else {
-                            const sortedEvents = events.sort(a, b => b.id - a.id);
-                            this.eventId = +sortedEvents[sortedEvents.length - 1].id + 1;
+                            const sortedEvents = events.sort((a, b) => b.id - a.id);
+                            this.eventId = +sortedEvents[0].id;
                         }
                     })
             });
