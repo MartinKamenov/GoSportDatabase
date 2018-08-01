b5ea3293426271cc20cfaf679f052c82507567d4
Martin Kamenov <martinkamenov10@gmail.com>
2018-08-01 20:42:12 +0300

    Exposed profile picture for users joining to event.

--- GoSportDB/routes/events/events-controller.js
+++ GoSportDB/routes/events/events-controller.js
@@ -140,6 +140,7 @@ const controller = {
                 email: user.email,
                 username: user.username,
                 city: user.city,
+                profileImg: user.profileImg,
                 password: user.password
             };
 
